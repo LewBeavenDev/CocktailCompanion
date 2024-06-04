177740c1fdf1690e88af147eb37110af8eae23e5
LewBeavenDev <elu95@hotmail.co.uk>
2024-06-04 11:52:42 +0100

    added modal to delete function

--- thecocktailcompanion/models.py
+++ thecocktailcompanion/models.py
@@ -1,5 +1,16 @@
 from thecocktailcompanion import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from sqlalchemy import String
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    # schema for the User model
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
 
 class Drink(db.Model):
     # schema for the Drinks model
@@ -11,6 +22,7 @@ class Drink(db.Model):
     drink_method = db.Column(db.String(255), nullable=False)
     drink_ingredients = db.Column(db.Text, nullable=False)
     drink_garnish = db.Column(db.String(255), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     def __repr__(self):
         return self.drink_name, self.drink_glass, self.drink_ice, self.drink_method, self.drink_ingredients, self.drink_garnish
--- thecocktailcompanion/routes.py
+++ thecocktailcompanion/routes.py
@@ -1,8 +1,81 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, blueprint, flash
 from werkzeug.utils import secure_filename
 from thecocktailcompanion import app, db
 from thecocktailcompanion.models import Drink
+from models import db, User, Drink
+from thecocktailcompanion.forms import RegistrationForm, LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('main.login'))
+    return render_template('register.html', form=form)
+
+
+@main_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('main.dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
+
+
+@main_blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
+
+
+@main_blueprint.route('/dashboard')
+@login_required
+def dashboard():
+    drinks = Drink.query.filter_by(user_id=current_user.id).all()
+    return render_template('dashboard.html', drinks=drinks)
+
+
+@main_blueprint.route('/edit/<int:drink_id>', methods=['GET', 'POST'])
+@login_required
+def edit_drink(drink_id):
+    drink = Drink.query.get_or_404(drink_id)
+    if drink.user_id != current_user.id:
+        flash('You do not have permission to edit this drink', 'danger')
+        return redirect(url_for('main.dashboard'))
+    if request.method == "POST":
+        drink.drink_name = request.form.get("drink_name")
+        db.session.commit()
+        return redirect(url_for("drinks")) 
+    # ...
+    return render_template('edit_drink.html', drink=drink)
+
+@main_blueprint.route('/delete/<int:drink_id>', methods=['POST'])
+@login_required
+def delete_drink(drink_id):
+    drink = Drink.query.get_or_404(drink_id)
+    if drink.user_id != current_user.id:
+        flash('You do not have permission to delete this drink', 'danger')
+        return redirect(url_for('main.dashboard'))
+    db.session.delete(drink)
+    db.session.commit()
+    flash('Drink has been deleted', 'success')
+    return redirect(url_for('main.dashboard'))
+    
 
 UPLOAD_FOLDER = 'thecocktailcompanion/static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -62,19 +135,5 @@ def add_drink():
     return render_template('add_drink.html')
 
 
-@app.route('/edit_drink/<int:drink_id>', methods=["GET", "POST"])
-def edit_drink(drink_id):
-    drink = Drink.query.get_or_404(drink_id)
-    if request.method == "POST":
-        drink.drink_name = request.form.get("drink_name")
-        db.session.commit()
-        return redirect(url_for("drinks"))    
-    return render_template("edit_drink.html", drink=drink)
-    
-    
-@app.route('/delete_drink/<int:drink_id>')
-def delete_drink(drink_id):
-    drink = Drink.query.get_or_404(drink_id) 
-    db.session.delete(drink)
-    db.session.commit()
-    return redirect(url_for("drinks"))
+
+
