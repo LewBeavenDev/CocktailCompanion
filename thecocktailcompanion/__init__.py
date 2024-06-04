f89158b8b7bb8f23589c63483a53a449a81bc876
LewBeavenDev <elu95@hotmail.co.uk>
2024-06-04 12:06:16 +0100

    added modal to delete function

--- thecocktailcompanion/__init__.py
+++ thecocktailcompanion/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/thecocktailcompanion'  # Adjust this line
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/thecocktailcompanion'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
