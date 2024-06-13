a53db85c7a025ba1172b8acb7dc96de909dd5a68
LewBeavenDev <elu95@hotmail.co.uk>
2024-06-13 18:15:02 +0100

    Update run.py and add create_admin.py

---
+++ create_admin.py
@@ -0,0 +1,18 @@
+from thecocktailcompanion import create_app, db
+from thecocktailcompanion.models import User
+from werkzeug.security import generate_password_hash
+
+app = create_app()
+
+with app.app_context():
+    # Check if admin user exists
+    admin_user = User.query.filter_by(username="admin").first()
+    if not admin_user:
+        # Create admin user if it doesn't exist
+        admin_user = User(
+            username="admin",
+            password=generate_password_hash("password", method='pbkdf2:sha256'),
+            is_admin=True
+        )
+        db.session.add(admin_user)
+        db.session.commit()
