4bf82c4aca2865d8c12c20f73887c975cfeae6d5
LewBeavenDev <elu95@hotmail.co.uk>
2024-06-13 19:29:19 +0100

    Add UPLOAD_FOLDER config

--- thecocktailcompanion/__init__.py
+++ thecocktailcompanion/__init__.py
@@ -12,6 +12,8 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     
+    app.config['UPLOAD_FOLDER'] = 'thecocktailcompanion/static/uploads'
+    
     if os.environ.get("DEVELOPMENT") == "True":
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
     else:
