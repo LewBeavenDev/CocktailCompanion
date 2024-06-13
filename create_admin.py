5ffd7d31191d3dc6c41fb360676ee896e5838c87
LewBeavenDev <elu95@hotmail.co.uk>
2024-06-13 18:38:03 +0100

    Add initialize_db script

--- create_admin.py
+++ create_admin.py
@@ -5,10 +5,8 @@ from werkzeug.security import generate_password_hash
 app = create_app()
 
 with app.app_context():
-    # Check if admin user exists
     admin_user = User.query.filter_by(username="admin").first()
     if not admin_user:
-        # Create admin user if it doesn't exist
         admin_user = User(
             username="admin",
             password=generate_password_hash("password", method='pbkdf2:sha256'),
@@ -16,3 +14,6 @@ with app.app_context():
         )
         db.session.add(admin_user)
         db.session.commit()
+        print("Admin user created.")
+    else:
+        print("Admin user already exists.")
