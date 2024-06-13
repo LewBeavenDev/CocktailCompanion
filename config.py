7e607c47c68d2aadff27f55ebceb230b1b6ad7a9
LewBeavenDev <elu95@hotmail.co.uk>
2024-06-13 17:11:37 +0100

    Use HEROKU_POSTGRESQL_URL for database configuration

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fe79d3f6e6dac0f7b94289aae8bfebcdd8f65ba102db43cc93f1ec55a6fed4c3'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@localhost/thecocktailcompanion'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_URL') or 'postgresql://postgres:postgres@localhost/thecocktailcompanion'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'thecocktailcompanion', 'static', 'uploads')
 
