0827ae39c56fca63520a28b8eba1a130538659c3
LewBeavenDev <elu95@hotmail.co.uk>
2024-10-16 12:18:39 +0100

    Update run.py to bind to correct port

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from thecocktailcompanion import create_app, db
 from thecocktailcompanion.models import User
 from werkzeug.security import generate_password_hash
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)
